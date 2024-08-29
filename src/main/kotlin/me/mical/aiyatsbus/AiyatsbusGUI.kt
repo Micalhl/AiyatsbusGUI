5c2b0fa24a219f44a0c604448d82898bf9c4f269
Micalhl <m@taboo.sh>
2024-08-29 17:01:44 +0800

    * update dev commands

--- src/main/kotlin/me/mical/aiyatsbus/AiyatsbusGUI.kt
+++ src/main/kotlin/me/mical/aiyatsbus/AiyatsbusGUI.kt
@@ -1,6 +1,11 @@
 package me.mical.aiyatsbus
 
+import me.mical.aiyatsbus.ui.MainMenu
+import org.bukkit.entity.Player
 import taboolib.common.platform.Plugin
+import taboolib.common.platform.command.CommandBody
+import taboolib.common.platform.command.CommandHeader
+import taboolib.common.platform.command.mainCommand
 import taboolib.common.platform.function.info
 
 /**
@@ -10,9 +15,15 @@ import taboolib.common.platform.function.info
  * @author mical
  * @date 2024/8/29 15:09
  */
+@CommandHeader(name = "agui")
 object AiyatsbusGUI : Plugin() {
 
     override fun onEnable() {
         info("Successfully running ExamplePlugin!")
+    }
+
+    @CommandBody
+    val main = mainCommand {
+        execute<Player> { sender, _, _ -> MainMenu.openMenu(sender) }
     }
 }
